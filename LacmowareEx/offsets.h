4ba8d7f415cc57815f2ed391bb6e4fafd69de8b8
ggpabuk <59561857+ggpabuk@users.noreply.github.com>
2022-11-25 17:06:43 +0300

    (fix) offsets update

--- LacmowareEx/offsets.h
+++ LacmowareEx/offsets.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define OUTDATED 0
+
 #include <Windows.h>
 #include <vector>
 
@@ -9,15 +11,15 @@ namespace offsets
 {
     const std::vector<offset_t> base = { 0x11C, 0x00 };
 
-    const offset_t gunContainer   = -0x1E8; // fixed
-    const offset_t visuals        = 0xE14; // fixed
-    const offset_t playerStats    = 0xB14; // fixed
-    const offset_t maynardKeycode = 0xAEC; // fixed
-    const offset_t wireframe      = 0x5B8; // fixed
-    const offset_t playerList     = -0x890;
-    const offset_t currentGunId   = -0x824;
-    const offset_t serverInfo     = -0x44; // fixed
-    const offset_t intercom       = -0x5D0;
-    const offset_t scpStats       = -0x5CC;
-    const offset_t gammaUpdate    = 0x4E0; // fixed
+    const offset_t gunContainer   = -0xF8; // fixed
+    const offset_t visuals        = 0xF24; // fixed
+    const offset_t playerStats    = 0xC24; // fixed
+    const offset_t maynardKeycode = 0xBFC; // fixed
+    const offset_t wireframe      = 0x6C8; // fixed
+    const offset_t playerList     = OUTDATED;
+    const offset_t currentGunId   = OUTDATED;
+    const offset_t serverInfo     = 0x138; // fixed
+    const offset_t intercom       = OUTDATED;
+    const offset_t scpStats       = OUTDATED;
+    const offset_t gammaUpdate    = 0x5F0; // fixed
 }
