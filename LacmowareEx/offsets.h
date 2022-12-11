6caba726b7c8397f9cc5a91bda2a09a7599d2e80
ggpabuk <59561857+ggpabuk@users.noreply.github.com>
2022-12-11 12:51:14 +0300

    (fix) offsets updated

--- LacmowareEx/offsets.h
+++ LacmowareEx/offsets.h
@@ -11,15 +11,15 @@ namespace offsets
 {
     const std::vector<offset_t> base = { 0x11C, 0x00 };
 
-    const offset_t gunContainer   = -0xF8; // fixed
-    const offset_t visuals        = 0xF24; // fixed
-    const offset_t playerStats    = 0xC24; // fixed
-    const offset_t maynardKeycode = 0xBFC; // fixed
-    const offset_t wireframe      = 0x6C8; // fixed
+    const offset_t gunContainer   = 0x2464;
+    const offset_t visuals        = 0xE50;
+    const offset_t playerStats    = 0xB4C;
+    const offset_t maynardKeycode = 0xB24;
+    const offset_t wireframe      = 0x5E8;
     const offset_t playerList     = OUTDATED;
     const offset_t currentGunId   = OUTDATED;
-    const offset_t serverInfo     = 0x138; // fixed
+    const offset_t serverInfo     = 0x44;
     const offset_t intercom       = OUTDATED;
     const offset_t scpStats       = OUTDATED;
-    const offset_t gammaUpdate    = 0x5F0; // fixed
+    const offset_t gammaUpdate    = 0x510;
 }
